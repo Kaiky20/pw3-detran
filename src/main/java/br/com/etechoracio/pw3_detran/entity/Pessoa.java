8a73c916684712cb069273447a5a1e65c463a43e
kaiky <kaikyribeirocorreia@gmail.com>
2022-04-06 14:22:47 -0300

    Criacao da entidade Pessoa.

---
+++ src/main/java/br/com/etechoracio/pw3_detran/entity/Pessoa.java
@@ -0,0 +1,25 @@
+package br.com.etechoracio.pw3_detran.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+@Table
+@Entity
+@Getter
+@Setter
+public class Pessoa {
+	
+	@Column (name = "IdPessoa")
+	@Id
+	int id;
+	@Column (name = "nome")
+	String nome;
+	@Column(name = "endereco") 
+	String endereco;
+	
+
+}
