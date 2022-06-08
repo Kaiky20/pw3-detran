44b5c515842f4f89d4e31ebacab50447d28e602c
rennan <rennan@email.com>
2022-06-08 14:18:26 -0300

    adicionado springs validation para validação de campos

--- src/main/java/br/com/etechoracio/pw3_detran/entity/Pessoa.java
+++ src/main/java/br/com/etechoracio/pw3_detran/entity/Pessoa.java
@@ -4,6 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +19,17 @@ public class Pessoa {
 	
 	@Column (name = "IdPessoa")
 	@Id
+	@NotBlank
 	int id;
 	@Column (name = "nome")
+	@NotNull (message= "campo obrigatorio")
+	@NotBlank
+	@Length(max=150)
 	String nome;
 	@Column(name = "endereco") 
+	@NotNull 
+	@NotBlank
+	@Length(max=150)
 	String endereco;
 	
 
